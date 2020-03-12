208b0dd511fe43a46cc89e4087f83f2572bfb348
yangis1019 <50739218+yangis1019@users.noreply.github.com>
2020-03-12 18:21:58 +0900

    상세 주소로 찾을 수 있도록 대폭 수정

--- main.py
+++ main.py
@@ -1,51 +1,48 @@
 import json
 from urllib.request import urlretrieve
-from urllib.parse import quote, unquote
+import requests
 
-addr = quote(input("조회하고 싶은 지역을 입력하세요: "))
+try:
+    addr = input("주소를 입력하세요: ")
+    api_key = "9cc93af28cb451da16bfa4dccb01f62f"
+    res = requests.get("https://dapi.kakao.com/v2/local/search/address.json", headers={"Authorization":"KakaoAK "+api_key}, params={"query":addr})
+    data = json.loads(res.text)
+    
+    x = data["documents"][0]["address"]["x"]
+    y = data["documents"][0]["address"]["y"]
+except IndexError:
+    print("잘못된 주소입니다.")
+    input("엔터 키로 종료합니다...")
+    exit(1)
 
-urlretrieve("https://8oi9s0nnth.apigw.ntruss.com/corona19-masks/v1/storesByAddr/json?address="+addr, "./data.json")
+m = input("몇m 주변을 검색할지 입력하세요: ")
+
+urlretrieve("https://8oi9s0nnth.apigw.ntruss.com/corona19-masks/v1/storesByGeo/json?lat="+y+"&lng="+x+"&m"+m, "./data.json")
 
 with open("./data.json", "r", encoding='utf-8') as f:
     data = json.load(f)
 
-print("데이터를 성공적으로 불러왔습니다. " + unquote(addr) + "에 있는 공적 마스크 판매장소는 총 " + str(data["count"]) + "개 입니다.")
 
-try:
-    n = int(input("번호를 입력하세요: "))
-except ValueError:
-    print("숫자가 아닙니다.")
-    exit(1)
-n -= 1
+print("데이터를 성공적으로 불러왔습니다. " + addr + "(으)로부터 "+ m +"미터 이내에 있는 공적 마스크 판매장소는 총 " + str(data["count"]) + "개 입니다.")
 
-try:
-    print("주소: " + data["stores"][n]["addr"])
-except IndexError:
-    print("오류 발생. 주소나 숫자를 정확하게 입력하였는지 다시 한번 확인하여 주세요.")
-    exit(1)
+print("|  주소  |  명칭  |  남은 마스크 수  |")
 
-print("명칭: " + data["stores"][n]["name"])
-if data["stores"][n]["remain_stat"] == "plenty":
-    print("남은 마스크 수: 100개 이상")
-elif data["stores"][n]["remain_stat"] == "some":
-    print("남은 마스크 수: 30개 이상 100개 미만")
-elif data["stores"][n]["remain_stat"] == "few":
-    print("남은 마스크 수: 2개 이상 30개 미만")
-elif data["stores"][n]["remain_stat"] == None:
-    print("남은 마스크의 수를 알 수 없습니다.")
-else:
-    print("해당 판매처의 마스크가 매진되었습니다.")
+for i in range(0, data["count"]):
+    print("|  ", end="")
+    print(data["stores"][i]["addr"], end="")
+    print("  |  ", end="")
+    print(data["stores"][i]["name"], end="")
+    print("  |  ", end="")
+    if data["stores"][i]["remain_stat"] == "plenty":
+        print("100개 이상", end="")
+    elif data["stores"][i]["remain_stat"] == "some":
+        print("30~99개", end="")
+    elif data["stores"][i]["remain_stat"] == "few":
+        print("2~29개", end="")
+    elif data["stores"][i]["remain_stat"] == None:
+        print("알 수 없음", end="")
+    else:
+        print("0~1개", end="")
+    print("  |")
 
-store_type = ["", "약국", "우체국", "농협"]
-
-print("판매처 유형: " + store_type[int(data["stores"][n]["type"])])
-
-if data["stores"][n]["stock_at"] == None:
-    print("마지막으로 입고된 시간을 알 수 없습니다.")
-else:
-    print("마지막으로 입고된 시간: " + data["stores"][n]["stock_at"])
-
-if data["stores"][n]["created_at"] == None:
-    print("마지막으로 해당 데이터가 업데이트된 시간을 알 수 없습니다.")
-else:
-    print("마지막으로 해당 데이터가 업데이트된 시간: " + data["stores"][n]["created_at"])
+input("엔터 키로 종료합니다...")
